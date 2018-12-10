cecd500a0f24700b7df67deb7e749176ca24d1be
DimaCrafter <backup.dimacrafter@gmail.com>
2018-12-10 17:17:42 +0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,30 @@
+# MongoDB driver for `dc-api-core`
+
+[![NPM](https://nodei.co/npm/dc-api-mongo.png)](https://npmjs.com/package/dc-api-mongo)
+
+## Dependencies
+
+* [dc-api-core](https://github.com/DimaCrafter/dc-api-core)
+* [Mongoose](https://github.com/Automattic/mongoose)
+* [mongoose-auto-increment](https://github.com/codetunnel/mongoose-auto-increment)
+
+---
+
+## Easy installation
+
+1) Install package - `npm i dc-api-mongo --save` or `yarn add dc-api-mongo`
+2) Add `dc-api-mongo` to `plugins` array in `config.json`
+3) Fill `db` field in `config.json` by template
+4) Done!
+
+---
+
+## `config.json` template
+
+| Field           | Default   | Description       |
+|-----------------|-----------|-------------------|
+| `db.mongo.host` | Required  | Database hostname |
+| `db.mongo.name` | Required  | Database name     |
+| `db.mongo.port` | `27017`   | Database port     |
+| `db.mongo.user` | Optional  | Database username |
+| `db.mongo.pass` |           | and password      |
---
+++ index.js
@@ -0,0 +1,58 @@
+const ROOT = process.cwd();
+const mongoose = require('mongoose');
+const {Schema} = mongoose;
+const mongooseAI = require('mongoose-auto-increment');
+
+class MongoDB {
+    constructor(conf) {
+        !conf.port && (conf.port = 27017);
+        const uri = `mongodb://${conf.user?`${conf.user}:${conf.pass}@`:''}${conf.host}:${conf.port}/${conf.name}`;
+        this.conn = mongoose.createConnection(uri, {
+            useCreateIndex: true,
+            useNewUrlParser: true
+        });
+        mongooseAI.initialize(this.conn);
+    }
+
+    getModel (name) {
+        if(name in this.conn.models) return this.conn.models[name];
+        try {
+            var schemaRaw = {...require(`${ROOT}/models/mongo/${name}.js`)};
+        } catch {
+            console.log('[MongoDB] Model `' + name + '` not found');
+            return;
+        }
+
+        let schemaData = {
+            virtuals: schemaRaw.virtuals || {}
+        };
+
+        delete schemaRaw.virtuals;
+        // Parsing auto-increment options
+        switch(typeof schemaRaw.increment) {
+            case 'string':
+                schemaData.increment = {
+                    model: name,
+                    field: schemaRaw.increment
+                };
+                break;
+            case 'object':
+                schemaData.increment = {...schemaRaw.increment, ...{model: name}};
+                break;
+        }
+        delete schemaRaw.increment;
+
+        const schema = new Schema(schemaRaw);
+        // Parsing virtuals
+        Object.keys(schemaData.virtuals).forEach(key => {
+            schema.virtual(key)
+                .get(schemaData.virtuals[key].get)
+                .set(schemaData.virtuals[key].set);
+        });
+        // Enabling auto-increment plugin if necessary
+        schemaData.increment && schema.plugin(mongooseAI.plugin, schemaData.increment);
+        return this.conn.model(name, schema);
+    }
+}
+
+module.exports = core => core.register('db', MongoDB, 'mongo');
